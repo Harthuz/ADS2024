ae6efa50b8e48426b0c8239fb73f155f1b4ec940
Harthuz <hernandeshass910@gmail.com>
2024-03-14 00:05:22 -0300

    add estrutura For

---
+++ 1° Bimestre/Aula 0307/estruturaFor/estruturaFor2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página estruturaFor2.php</title>
+</head>
+<body>
+    <?php
+        for($i = 5; $i >=1; $i--){
+            echo $i .'<br />';
+        }
+    ?>
+</body>
+</html>
